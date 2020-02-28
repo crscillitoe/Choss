b4981b85ad6e80993d57ad5057ac3e592f573221
crscillitoe <christian.scillitoe@gmail.com>
2020-02-27 23:34:14 -0800

    add move definitions for horizontal/vertical moves

---
+++ shared/chesslib/MoveRules/HorizontalMove.ts
@@ -0,0 +1,19 @@
+import { MoveRule } from "../MoveRule";
+import { Board } from "../Board";
+import { Piece } from "../Piece";
+import { Coordinate } from "../Coordinate";
+
+/**
+ * This rule, when applied to a piece, will stop it from performing
+ * any movement that involves jumping over a piece.
+ */
+export class HorizontalMove implements MoveRule {
+  distance: number = -1;
+  constructor(distance: number) {
+    this.distance = distance;
+  }
+
+  ValidSqures(piece: Piece, board: Board): Coordinate[] {
+    throw new Error("Not Implemented");
+  }
+}
