1e16f34e3189af89fd7005f2f5b8a62f73a161cb
Christian Scillitoe <christian.scillitoe@gmail.com>
2021-08-14 04:59:32 +0000

    Choss

--- front-end-lichess/projects/chess/src/localConfiguration.ts
+++
@@ -1,3 +0,0 @@
-import { RandomAtomic } from "./lib/chesslib/GameModes/RandomAtomic";
-export const socketIp = "dev-api.woohoojin.dev";
-export const boardGame = new RandomAtomic();
--- front-end-lichess/src/app/app.component.ts
+++ front-end-lichess/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { DoubleMove } from 'projects/chess/src/lib/chesslib/GameModes/DoubleMove';
+import Chessground from 'projects/chessground/src';
+import { Api } from 'projects/chessground/src/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('chess')
+  chess!: ElementRef;
+  api!: Api;
+
+  ngAfterViewInit(): void {
+    const db = new DoubleMove();
+    this.api = Chessground(this.chess.nativeElement, db);
+    this.api.move('e2', 'e4');
+  }
+
   title = 'front-end-lichess';
 }
