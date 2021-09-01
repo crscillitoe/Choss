e2a6a08115635e0b6fe1c8e126fe85c588819330
Christian Scillitoe <christian.scillitoe@gmail.com>
2021-09-01 21:13:00 +0000

    stuff

--- front-end/build.sh
+++ front-end/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+echo "[------ Building ------]"
 ng build --prod
+echo "[------ Updating Prod Files ------]"
 cd /var/www/choss
 rm -rf front-end/
 mv ~/git/Choss/front-end/dist/front-end .
+echo "[------ Done! ------]"
